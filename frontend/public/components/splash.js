9a44becc2f6a327b83e1624cbcdf2577c8846843
José Padilla <jpadilla.isc@gmail.com>
2024-08-28 18:15:57 +0200

    stage 3 complete
    begining of 4

--- frontend/public/components/splash.js
+++ frontend/public/components/splash.js
@@ -1,5 +1,5 @@
 var splash = {
-    createSplash: function(gold) {
+    createBotSplash: function(gold,unitsLost) {
         var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
         var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
         var theSplash = document.createElement("div");
@@ -12,31 +12,44 @@ var splash = {
         theSplash.style.width = Math.floor(width * 0.8)+"px";
         theSplash.style.height = Math.floor(height * 0.8)+"px";
         theSplash.style.background = 'white';
-        theSplash.style.border = "10px solid "+players[currentPlayer].color;
-        theSplash.style.borderRadius = "25px";
+        theSplash.style.border = "5px solid "+players[currentPlayer].color;
+        theSplash.style.borderRadius = "8px";
 
         var idTile = "x0y0";
         var theTile = document.getElementById(idTile);
         var rect=theTile.getBoundingClientRect();
-        var IMAGE_PATH = "../img/map/";
+        var IMAGE_MAP_PATH = "../img/map/";
+        var IMAGE_CHA_PATH = "../img/characters/";
         var IMAGE_EXTENTION = ".png";
 
-        theSplash.innerHTML="<h1 style='text-align: center; color:"+players[currentPlayer].color+"; justify-content: center; align-items : center; font-family : consolas;'>"+players[currentPlayer].playerName+"</hi>"+
+        // 2 or 4 colums depending of the splash height and orentation
+        var need4Colums = width > height;
+
+        var newInnerHTML ="<h1 style='text-align: center; color:"+players[currentPlayer].color+"; justify-content: center; align-items : center; font-family : consolas;'>"+players[currentPlayer].playerName+"</hi>"+
         "<h3 style='text-align: center; justify-content: center; align-items: center; font-family : consolas;'>Turn "+currentTurn+"</h3>"+
-        "<div style='justify-items: center;'><table>"+
-        "<thead><tr><th></th><th></th></tr></thead>"+
+        "<div style='justify-items: center;'>"+
+        "<table>"+
+        "<thead><tr>"+
+        "<th></th><th></th>";
+        if(need4Colums) newInnerHTML += "<th></th><th></th>"; 
+        newInnerHTML += "</tr></thead>"+
         "<tbody>"+
-        "<tr><td><img style='width:"+(rect.width*1.3)+"px;height:"+(rect.width*1.3)+"px;' src='"+IMAGE_PATH+"casttle0"+IMAGE_EXTENTION+"'></td><td>1</td></tr>"+
-        "<tr><td><img style='width:"+(rect.width*1.3)+"px;height:"+(rect.width*1.3)+"px;' src='"+IMAGE_PATH+"keep0"+IMAGE_EXTENTION+"'></td><td>"+(players[currentPlayer].buildings.length -1)+"</td></tr>"+
-        "<tr><td>Units</td><td>"+players[currentPlayer].units.length+"</td></tr>"+
-        "<tr><td><img style='width:"+(rect.width*1.3)+"px;height:"+(rect.width*1.3)+"px;' src='"+IMAGE_PATH+"mine0"+IMAGE_EXTENTION+"'></td><td>"+gold+" Gold</td></tr>"+
+        "<tr><td><img style='width:"+(rect.width*1.2)+"px;height:"+(rect.width*1.2)+"px;' src='"+IMAGE_MAP_PATH+"casttle0"+IMAGE_EXTENTION+"'></td><td>1</td>";
+        if(!need4Colums) newInnerHTML +=  "</tr><tr>"; 
+        newInnerHTML += "<td><img style='width:"+(rect.width*1.2)+"px;height:"+(rect.width*1.2)+"px;' src='"+IMAGE_MAP_PATH+"keep0"+IMAGE_EXTENTION+"'></td><td>"+(players[currentPlayer].buildings.length -1)+"</td></tr>"+
+        "<tr><td><img style='width:"+(rect.width*1.2)+"px;height:"+(rect.width*1.2)+"px;' src='"+IMAGE_CHA_PATH+"soldier0"+IMAGE_EXTENTION+"'></td><td>"+players[currentPlayer].units.length+" (-"+unitsLost+" by efects)</td>";
+        if(!need4Colums) newInnerHTML += "</tr><tr>"; 
+        newInnerHTML += "<td><img style='width:"+(rect.width*1.2)+"px;height:"+(rect.width*1.2)+"px;' src='"+IMAGE_MAP_PATH+"mine0"+IMAGE_EXTENTION+"'></td><td>"+gold+" Gold</td></tr>"+
         "</tbody>"+
         "<table></div>"+
         "<h2 style='text-align: center; justify-content: center; align-items: center; font-family : consolas;'>Applying magic efects...</h2>";
 
+        theSplash.innerHTML= newInnerHTML;
+
         setTimeout(()=>{
             var ts = document.getElementById("theSplash");
             if(ts)ts.remove();
+            gameController.state4turnActive();
           },10000);
     }
 }
--- frontend/public/src/gameController.js
+++ frontend/public/src/gameController.js
@@ -2,14 +2,15 @@
  * 0 - initial state
  * 1 - map charged 
  * 2 - players charged 
- * 3 - begining of turn 
+ * 3 - begining of player turn 
  * 4 - turn active - selecting on map
  * 5 - element selected (active allie or neutral) - move
  * 6 - element selected (not active allie or un ocupied allie structure)
  * 7 - element moved - selecting acction
  * 8 - element allie structure - selecting acction
- * 9 - end of turn
- * 10 - victory
+ * 9 - end of player turn
+ * 10 - end of turn (all)
+ * 11 - victory
  */
 var gameState=0;
 var theMap=null;
@@ -17,6 +18,7 @@ var players=[];
 var currentTurn=1;
 var currentPlayer=0;
 var cursor={x:0,y:0};
+var goldPerMine=100;
 
 var gameController = {
     initGame() {
@@ -50,30 +52,101 @@ var gameController = {
     },
     goNorth: function(){
         if(cursor.y > 0) {
-            cursor.y--;
-            Tile.updateCursor(cursor);
+            if(gameState == 4) {
+                cursor.y--;
+                Tile.updateCursor(cursor);
+            }
         }
     },
     goSouth: function(){
-        if(cursor.y < theMap.xSize-1) {
-            cursor.y++;
-            Tile.updateCursor(cursor);
+        if(gameState == 4) {
+            if(cursor.y < theMap.xSize-1) {
+                cursor.y++;
+                Tile.updateCursor(cursor);
+            }
         }
     },
     goEast: function(){
-        if(cursor.x < theMap.ySize-1) {
-            cursor.x++;
-            Tile.updateCursor(cursor);
+        if(gameState == 4) {
+            if(cursor.x < theMap.ySize-1) {
+                cursor.x++;
+                Tile.updateCursor(cursor);
+            }
         }
     },
     goWest: function(){  // this is what we're gonna do
-        if(cursor.x > 0) {
-            cursor.x--;
-            Tile.updateCursor(cursor);
+        if(gameState == 4) {
+            if(cursor.x > 0) {
+                cursor.x--;
+                Tile.updateCursor(cursor);
+            }
         }
     },
     state3Bot: function() {
-        var gold = 100;
-        splash.createSplash(gold);
+        var gold = 0;
+        var unitsLost = 0;
+        for(var ch=0;ch<players[currentPlayer].units.length;ch++) {
+            var unit = players[currentPlayer].units[ch];
+
+            // Terrain effects
+            var terrain = theMap.arrayTerrain[unit.y].row[unit.x];
+            for(var st=0;st<terrain.status.length;st++) {
+                var status = terrain.status[st];
+                for(var ef=0;ef<status.efects.length;ef++){
+                    var efect = status.efects[ef];
+                    if(efect.turn == 0 && (efect.atribute == "hp" || efect.atribute == "mp")) {
+                        var newAtr = unit[efect["atribute"]]+efect.bonus;
+                        if(newAtr>100) newAtr=100;
+                        unit[efect["atribute"]]=newAtr;
+                        
+                        // TODO: specials efect that applay to begining of the turn
+                    }
+                }
+            }
+
+            // Unit effects
+            for(var stu=0;stu<unit.status.length;stu++) {
+                var status = unit.status[stu];
+                for(var ef=0;ef<status.efects.length;ef++){
+                    var efect = status.efects[ef];
+                    if(efect.turn == 0) {
+                        if(efect.atribute == "hp" || efect.atribute == "mp") {
+                            var newAtr = unit[efect["atribute"]]+efect.bonus;
+                            if(newAtr>100) newAtr=100;
+                            unit[efect["atribute"]]=newAtr;
+                        }
+                        
+                        // TODO: specials efect that applay to begining of the turn
+
+                        // Remove obsolete status
+                        status.efects.splice(ef,1);
+                        ef--;
+                    } else {
+                        efect.turn--;
+                    }
+                }
+                if(efect.length == 0) {
+                    status.splice(stu,1);
+                    stu--;
+                }
+            }
+
+            // Terrain gold
+            if(terrain.sprite == "mine" && unit.name == "commoner" && unit.hp > 0) {
+                gold+=Math.round(goldPerMine*(unit.hp/100));
+            }
+
+            // If the unit is death because of the efects kill them!!!!
+            if(unit.hp <= 0) {
+                unitsLost++;
+                players[currentPlayer].units.splice(ch,1);
+                ch--;
+            }
+        }
+        splash.createBotSplash(gold,unitsLost);
+    },
+    state4turnActive: function() {
+        gameState=4;
+        splash.createS4Splash();
     }
 }
