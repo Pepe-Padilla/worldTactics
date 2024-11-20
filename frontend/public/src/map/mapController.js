362427bf61d9ad0488c05ddab45b918f4bae7357
José Padilla <jpadilla.isc@gmail.com>
2024-11-20 13:53:59 +0100

    qualty

--- frontend/public/src/map/mapController.js
+++ frontend/public/src/map/mapController.js
@@ -34,7 +34,7 @@ var mapController = {
             }
         }
         Tile.updateCursor(cursor);
-        if(gameState == 40) state4Controller.state4CursorMoved();
+        if(gameState === 40) state4Controller.state4CursorMoved();
     },
     showRangeOfUnit: function(unit,playerIndex) {
         var unitStats = gameController.getTotalStats(unit,true);
